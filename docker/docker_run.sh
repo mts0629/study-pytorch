428499946f053e7a8ac5f61bcbfc413ec43802fe
mts0629 <soratobi93@gmail.com>
2024-07-27 22:28:28 +0900

    Move the container runner script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
-docker_image=pytorch/pytorch:2.3.0-cuda12.1-cudnn8-runtime
+docker_image=$1
 
+# Login as the same user with the host
 docker run -u $(id -u ${USER}):$(id -g ${USER}) \
     -v /etc/group:/etc/group:ro \
     -v /etc/passwd:/etc/passwd:ro \
