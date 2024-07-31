58ace8580b559373609882d44a16531168b7ee82
mts0629 <soratobi93@gmail.com>
2024-07-31 14:50:12 +0000

    Simple CNN: plot train/test histories

--- simple_cnn.py
+++ simple_cnn.py
@@ -4,6 +4,9 @@ import torch
 
 import torch.nn.functional as F
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 from torch import nn
 from torch.utils.data import DataLoader, RandomSampler
 from torchvision import datasets
@@ -49,6 +52,7 @@ def train(dataloader, device, model, loss_fn, optimizer):
 
     model.train()
 
+    total_loss, correct = 0, 0
     for i, (x, t) in enumerate(dataloader):
         x, t = x.to(device), t.to(device)
 
@@ -60,8 +64,19 @@ def train(dataloader, device, model, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
 
+        total_loss += loss.item()
+        correct += (y.argmax(1) == t).type(torch.float).sum().item()
+
         if i % print_loss_iter == 0:
-            print(f"Train loss: {loss.item():.7f} ({(i + 1)*len(x)}/{size})")
+            print(
+                f"Train loss: {total_loss/(i + 1):.7f} "
+                f"({(i + 1)*len(x)}/{size})"
+            )
+
+    total_loss /= size
+    correct /= size
+
+    return total_loss, correct
 
 
 def test(dataloader, device, model, loss_fn):
@@ -83,6 +98,24 @@ def test(dataloader, device, model, loss_fn):
     loss /= num_batches
     correct /= size
     print(f"Test accuracy: {(100*correct):.2f}[%], test loss: {loss:.7f}")
+
+    return loss, correct
+
+
+def plot_histories(train_history, test_history, ylabel):
+    """Plot train/test histories."""
+    fig, ax = plt.subplots()
+
+    t = np.linspace(1, len(train_history), len(train_history))
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel(ylabel)
+    ax.plot(t, train_history, label="Train")
+    ax.plot(t, test_history, label="Test")
+    ax.legend()
+
+    fig.tight_layout()
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -124,14 +157,36 @@ if __name__ == "__main__":
     # Momentum SGD
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
 
+    # Train/test history
+    history = {
+        "train": {"losses": [], "accs": []},
+        "test": {"losses": [], "accs": []},
+    }
+
     EPOCHS = 10
     for epoch in range(EPOCHS):
         print(f"--------------------")
         print(f"Epoch {epoch + 1}")
         print(f"--------------------")
 
-        train(train_dataloader, device, model, loss_fn, optimizer)
-        test(test_dataloader, device, model, loss_fn)
+        train_loss, train_acc = train(
+            train_dataloader, device, model, loss_fn, optimizer
+        )
+        test_loss, test_acc = test(test_dataloader, device, model, loss_fn)
+
+        history["train"]["losses"].append(train_loss)
+        history["test"]["losses"].append(train_loss)
+
+        history["train"]["accs"].append(train_acc)
+        history["test"]["accs"].append(train_acc)
+
+    # Plot histories
+    plot_histories(
+        history["train"]["losses"], history["test"]["losses"], "Loss"
+    )
+    plot_histories(
+        history["train"]["accs"], history["test"]["accs"], "Accuracy"
+    )
 
     MODEL_PATH = "work/simplecnn.pth"
     torch.save(model.state_dict(), MODEL_PATH)
