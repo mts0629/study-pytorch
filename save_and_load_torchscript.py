ff79a3aca63380e17922f8a95500a4dbe30776d6
mts0629 <soratobi93@gmail.com>
2024-08-07 23:59:49 +0900

    Use Torchscript scripting

--- save_and_load_torchscript.py
+++ save_and_load_torchscript.py
@@ -9,25 +9,26 @@ from torchvision import datasets
 from torchvision.transforms import v2  # Use transforms.v2 API
 
 
-class SimpleNN(nn.Module):
-    """Simple neural network."""
+def get_tracing():
+    """Get a TorchScript tracing."""
+    class SimpleNN(nn.Module):
+        """Simple neural network."""
 
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.l1 = nn.Linear(in_features=28*28, out_features=512)
-        self.l2 = nn.Linear(512, 512)
-        self.l3 = nn.Linear(512, 10)
+        def __init__(self):
+            super().__init__()
+            self.flatten = nn.Flatten()
+            self.l1 = nn.Linear(in_features=28*28, out_features=512)
+            self.l2 = nn.Linear(512, 512)
+            self.l3 = nn.Linear(512, 10)
 
-    def forward(self, x):
-        x = self.flatten(x)
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = self.l3(x)
-        return x
+        def forward(self, x):
+            x = self.flatten(x)
+            x = F.relu(self.l1(x))
+            x = F.relu(self.l2(x))
+            x = self.l3(x)
+            return x
 
 
-if __name__ == "__main__":
     transform = v2.Compose([
         v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True)
@@ -60,9 +61,57 @@ if __name__ == "__main__":
 
     # Save the TorchScript module
     # includes code, parameters, attributes and debug information
-    SCRIPT_PATH = "work/simplenn.pt"
-    traced_model.save(SCRIPT_PATH)
+    TRACE_PATH = "work/simplenn.pt"
+    traced_model.save(TRACE_PATH)
 
     # Load the module (to CPU)
-    pt_model = torch.jit.load(SCRIPT_PATH).to("cpu").eval()
+    pt_model = torch.jit.load(TRACE_PATH).to("cpu").eval()
     assert torch.equal(model(x), pt_model(x))
+
+
+def get_script():
+    """Get a TorchScript."""
+    class Gate(nn.Module):
+        """Gate module, fake RNN."""
+
+        def forward(self, x):
+            if x.sum() > 0:
+                return x
+            else:
+                return -x
+
+    class Cell(nn.Module):
+        """Cell module, fake RNN."""
+
+        def __init__(self, gate):
+            super().__init__()
+            self.gate = gate
+            self.linear = nn.Linear(4, 4)
+
+        def forward(self, x, h):
+            new_h = torch.tanh(self.gate(self.linear(x)) + h)
+            return new_h, new_h
+
+
+    x = torch.rand(3, 4)
+    h = torch.rand(3, 4)
+
+    cell = Cell(Gate())
+    print(cell)
+
+    traced_cell = torch.jit.trace(cell)
+    print(traced_cell.code)
+
+
+    scripted_gate = torch.jit.script(Gate())
+    scripted_cell = torch.jit.script(Cell(scripted_gate))
+
+    print(scripted_gate.code)
+    print(scripted_cell.code)
+
+
+
+if __name__ == "__main__":
+    # get_tracing()
+
+    get_script()
