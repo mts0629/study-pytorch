c3e0be70b9b57410bd378941d19a10979a77c085
mts0629 <soratobi93@gmail.com>
2024-09-28 00:19:05 +0900

    Get memory consumption

--- profiler.py
+++ profiler.py
@@ -18,9 +18,29 @@ if __name__ == "__main__":
 
     # Print stats with sorting by total CPU time
     print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
-
     # With input shapes
     print(
         prof.key_averages(group_by_input_shape=True)\
         .table(sort_by="cpu_time_total", row_limit=10)
+    )
+
+    # Get a profile with memory consumption
+    model = models.resnet18()
+    inputs = torch.randn(8, 3, 224, 224)
+
+    with profile(
+        activities=[ProfilerActivity.CPU],
+        profile_memory=True,
+        record_shapes=True
+    ) as prof:
+        with record_function("model_inference"):
+            model(inputs)
+
+    # Sort by a memory amount allocated by the operator itself
+    print(
+        prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10)
+    )
+    # Sort by a memory amount allocated by the operator and its children
+    print(
+        prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10)
     )
