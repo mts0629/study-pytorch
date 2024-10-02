8a81732ca784e26ab8e59dfebfd2e7829ba25dc2
mts0629 <soratobi93@gmail.com>
2024-10-02 23:48:35 +0900

    More Autograd and quantization

--- use_tensors.py
+++ use_tensors.py
@@ -21,28 +21,57 @@ if __name__ == "__main__":
         dtype=np.float32
     )
     x_from_arr = torch.from_numpy(array)
-    print(x_from_arr)
-
     array_from_x = x.numpy()
-    print(array_from_x)
+
+    assert torch.equal(x, x_from_arr)
+    assert np.all(array == array_from_x)
 
     # Autograd
     print("# Autograd")
-    y = torch.tensor(
-        [[5, 6],
-         [7, 8]],
+    w = torch.tensor(
+        [[0.1, 0.2],
+         [0.3, 0.4]],
         dtype=torch.float32,
         requires_grad=True
     )
+    b = torch.ones(2, requires_grad=True)
 
     x.requires_grad_()  # x.requires_grad = True
 
-    z = x * y
-    z = z.sum()  # Need to create scalar outputs
-    z.backward()
-    print(x.grad)
-    print(y.grad)
+    y = torch.matmul(x, w) + b
+    t = torch.zeros([2, 2])
+    print(y)
 
-    with torch.no_grad():
-        z = x * y
-    print(z.requires_grad)
+    loss = torch.nn.functional.binary_cross_entropy_with_logits(y, t)
+
+    loss.backward()  # Backward need to start from scalar output
+    print("Grads:")
+    print(x.grad)
+    print(w.grad)
+    print(b.grad)
+
+    with torch.no_grad():  # Disable gradient tracking
+        y = torch.matmul(x, w) + b
+
+    assert y.requires_grad == False
+
+    print("# Quantize")
+    x = torch.rand([5], dtype=torch.float32)
+    print(x)
+    print("# Per-Tensor")
+    q_x = torch.quantize_per_tensor(
+        x, scale=0.1, zero_point=-128, dtype=torch.qint8
+    )
+    print(q_x)
+    print("=", q_x.int_repr())
+
+    print("# Per-Channel")
+    q_x = torch.quantize_per_channel(
+        x,
+        scales=torch.Tensor([0.1, 0.125, 0.15, 0.175, 0.2]),
+        zero_points=torch.Tensor([-128, -64, -32, -16, -8]),
+        axis=0,
+        dtype=torch.qint8
+    )
+    print(q_x)
+    print("=", q_x.int_repr())
